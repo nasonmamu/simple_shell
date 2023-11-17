e5f423c5def19d567f049c75e0d203553a37c9a8
nasonmamu <nasonmamu94@gmail.com>
2023-11-17 14:26:24 -0800

    contains the file prototype

--- shell.h
+++ shell.h
@@ -21,11 +21,6 @@
 
 /* Global environemnt */
 extern char **environ;
-/* Global program name */
-char *name;
-/* Global history counter */
-int hist;
-
 /**
  * struct list_s - A new struct type defining a linked list.
  * @dir: A directory path.
